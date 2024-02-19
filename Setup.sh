d52367d5afcf6f990e37db9ad01eba9f98ba3c38
Akshay <jinmori58690@gmail.com>
2024-02-20 01:16:44 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -4,7 +4,7 @@ echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
 wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-386.tgz
-sufo tar -xvzf ngrok-v3-stable-linux-386.tgz
+sudo tar -xvzf ngrok-v3-stable-linux-386.tgz
 chmod +x ./ngrok
 echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
 rm -f .ngrok.log
